4a7a0880a4a87257d9c09712ba42a164b74cd78f
tunghoangxuan <tunghx.contact@gmail.com>
2023-01-29 22:27:18 +0700

    setup without webpack

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Box, InputBase, TextField, Typography } from "@mui/material";
+import { useState } from "react";
 
 function App() {
+  const [firstValue, setFirstValue] = useState("");
+  const [secondValue, setSecondValue] = useState("");
+  const handleInputChange = (name) => {
+    return (e) => {
+      console.log("name --->", name);
+      console.log("value --->", e.target.value);
+      const val = e.target.value;
+
+      if (name === "first") {
+        setFirstValue(val);
+      } else {
+        setSecondValue(val);
+      }
+    };
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
         >
-          Learn React
-        </a>
+          <TextField
+            onChange={handleInputChange("first")}
+            value={firstValue}
+            label="First value"
+            color="success"
+            focused
+          />
+          <TextField
+            onChange={handleInputChange("second")}
+            value={secondValue}
+            label="Second value"
+            color="success"
+            focused
+          />
+        </Box>
+        <Typography color="black">
+          Result: {firstValue + secondValue}
+        </Typography>
       </header>
     </div>
   );
